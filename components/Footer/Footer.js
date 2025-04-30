34fdf919375554cfd27ee0b2e2e6c9f5cf6a21d3
Rosovski <zbao4@stevens.edu>
2025-04-29 23:11:55 -0400

    add footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,9 +1,23 @@
 import { Box, Typography } from "@mui/material";
+import classes from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <Box>
-      <Typography variant="h3">Footer</Typography>
+    <Box className={classes.container}>
+      <Box className={classes.content}>
+        <Typography variant="body1">
+          @2025 Affilyit; All Rights Reserved.
+        </Typography>
+        <Box className={classes.links}>
+          <Typography variant="body2">Privacy</Typography>
+          <Typography variant="body2">Terms</Typography>
+          <Typography variant="body2">Cookie</Typography>
+        </Box>
+        <Box className={classes.socialmedias}>
+          <Typography variant="body1">Twitter</Typography>
+          <Typography variant="body1">Instagram</Typography>
+        </Box>
+      </Box>
     </Box>
   );
 }
